296daad4c5e85e626dde823d8d0c93fd0f36680e
Ivan_Vyargizov <vyargizov@gmail.com>
2021-10-16 19:18:19 +0400

    rm Even.java to games/Even.java

---
+++ src/main/java/hexlet/code/games/Even.java
@@ -0,0 +1,25 @@
+package hexlet.code;
+
+public class Even {
+
+    private static void printConditionsOfTheGame() {
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.\n");
+    }
+
+    public static void isEven() {
+        Greet.toGreet();
+        printConditionsOfTheGame();
+        Engine.initializationLoop(getArrayOfEquationAndAnswer());
+    }
+
+    private static String[][] getArrayOfEquationAndAnswer() {
+        String[][] arrayOfEquationAndAnswer = new String[Engine.getLengthOfArray()][Engine.getLengthOfArray()];
+        for (int i = 0; i <= Engine.getMaxIteration(); i++) {
+            int equationToSolve = Engine.getRandomNumber();
+            arrayOfEquationAndAnswer[Engine.getLineOfEquation()][i] = Integer.toString(equationToSolve);
+            arrayOfEquationAndAnswer[Engine.getLineOfAnswer()][i] = equationToSolve % 2 == 0 ? "yes" : "no";
+        }
+        return arrayOfEquationAndAnswer;
+    }
+
+}
