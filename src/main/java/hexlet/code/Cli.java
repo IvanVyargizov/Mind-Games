cbd8cc4125f05be34141570adc8527a7ab919c2c
Ivan_Vyargizov <vyargizov@gmail.com>
2021-10-25 07:50:58 +0400

    modified file for correct greeting output

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -5,10 +5,9 @@ import java.util.Scanner;
 public class Cli {
     public static void toGreet() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("May I have your name?");
+        System.out.println("Welcome to the Brain Games!\nMay I have your name?\n");
         String name = sc.nextLine();
-        System.out.println("Hello, " + name);
-        sc.close();
+        System.out.println("\nHello, " + name + "!");
     }
 }
 
