4344863d0611e3c08228ab2f279e37305c041bee
Ivan_Vyargizov <vyargizov@gmail.com>
2021-10-25 07:44:52 +0400

    modified file. logic move from deleted file Choice.java

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -5,12 +5,13 @@ import hexlet.code.games.Even;
 import hexlet.code.games.GCD;
 import hexlet.code.games.Progression;
 import hexlet.code.games.Prime;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Choice.toChoice();
-        switch (Choice.getChoice()) {
-            case "1" -> Greet.toGreet();
+        toChoice();
+        switch (choice) {
+            case "1" -> Cli.toGreet();
             case "2" -> Even.isEven();
             case "3" -> Calc.toCalc();
             case "4" -> GCD.isGCD();
@@ -19,5 +20,21 @@ public class App {
             case "0" -> System.out.println("See you again!");
             default -> System.out.println("Your choice is incorrect, please try again ;)");
         }
+    }
+
+    private static String choice;
+    private static void toChoice() {
+        System.out.println("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                2 - Even
+                3 - Calc
+                4 - GCD
+                5 - Progression
+                6 - Prime
+                0 - Exit""");
+        Scanner scChoice = new Scanner(System.in);
+        choice = scChoice.nextLine();
+        System.out.println("Your choice: " + choice + "\n");
     }
 }
